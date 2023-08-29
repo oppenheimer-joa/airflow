2b3118c3bd3c3bff753003a5f859eb05a271eec9
nayeon1107 <quf991107@gmail.com>
2023-08-29 17:31:55 +0900

    230829/na operator 작성

--- dags/IMDB/get_IMDBAwards.py
+++ dags/IMDB/get_IMDBAwards.py
@@ -9,6 +9,10 @@ import requests, json, os
 
 local_tz = pendulum.timezone("Asia/Seoul")
 SERVER_API = Variable.get("SERVER_API")
+EVENT_IDS = {'academy' : 'ev0000003',
+             'cannes': 'ev0000147',
+             'venice' : 'ev0000681',
+             'busan': 'ev0004044'}
 
 default_args = {
   'start_date': datetime(2023, 8, 28, tzinfo=local_tz),
@@ -29,16 +33,16 @@ dag = DAG(
 
 # 함수 정의
 ## 수상작 정보 크롤링 함수
-def imdb_data_load(event, start_year) :
-    api_url = f"http://{SERVER_API}/imdb/award?event={event}&year={start_year}"
+def imdb_data_load(event, year) :
+    api_url = f"http://{SERVER_API}/imdb/award?event={event}&year={year}"
     response = requests.get(api_url).json()
     print("응답 수행 완료")
     return response
     
 
 ## 정합성 체크 함수
-def imdb_file_check(event, start_year):  ### params : event,start_year
-    file_path = f"/api/datas/IMDb/imdb_{event}_{start_year}.json"
+def imdb_file_check(event, year):  ### params : event,year
+    file_path = f"/api/datas/IMDb/imdb_{event}_{year}.json"
     if os.path.isfile(file_path):
         size = os.path.getsize(file_path)
         if size >= 8000:
@@ -50,24 +54,39 @@ def imdb_file_check(event, start_year):  ### params : event,start_year
 # Operator 정의
 
 start = EmptyOperator(task_id = 'Start.task', dag = dag)
+finish_load = EmptyOperator(task_id = 'Load.Done', dag = dag)
 error = EmptyOperator(task_id = 'ERROR', dag = dag)
 finish = EmptyOperator(task_id = 'Finish.task', dag = dag)
 
-get_IMDBAwards = PythonOperator(task_id="Get.IMDB_Awards",
-                            python_callable=imdb_data_load,
-                            op_kwargs={
-                                "event" : "",
-                                "start_date": "{{execution_date.in_timezone('Asia/Seoul').strftime('%Y')}}"
-                                },
-                            dag=dag)
+# get_IMDBAwards = PythonOperator(task_id="Get.IMDB_Awards",
+#                             python_callable=imdb_data_load,
+#                             op_kwargs={
+#                                 "event" : "",
+#                                 "year": "{{execution_date.in_timezone('Asia/Seoul').strftime('%Y')}}"
+#                                 },
+#                             dag=dag)
 
-check_IMDBAwards = PythonOperator(task_id="Check.IMDB_Awards",
-                                  python_callable=imdb_file_check,
-                                  op_kwargs={
-                                  "event" : "",
-                                  "start_date": "{{execution_date.in_timezone('Asia/Seoul').strftime('%Y')}}"
-                                },
-                                  provide_context=True,
-                                  dag=dag)
+load_tasks = list()
+for event,code in EVENT_IDS.items() :
+    load_task = PythonOperator(task_id=f"Get.IMDB_{event}_data",
+                          python_callable=imdb_file_check,
+                          op_kwargs={
+                              "event" : "{{code}}",
+                              "year": "{{execution_date.in_timezone('Asia/Seoul').strftime('%Y')}}"
+                              },
+                          dag=dag)
+    load_tasks.append(load_task)
+
+
+check_tasks = list()
+for event,code in EVENT_IDS.items() :
+    check_task = PythonOperator(task_id=f"Check.IMDB_{event}_data",
+                          python_callable=imdb_data_load,
+                          op_kwargs={
+                              "event" : "{{code}}",
+                              "year": "{{execution_date.in_timezone('Asia/Seoul').strftime('%Y')}}"
+                              },
+                          dag=dag)
+    check_tasks.append(check_task)
 # Operator 배치
-start >> get_IMDBAwards >> check_IMDBAwards >> finish
+start >> load_tasks >> finish_load >> check_tasks >> [error, finish]    # 마지막 분기 수정해주세요!!
