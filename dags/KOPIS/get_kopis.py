b6b9c61434607dfc4c4795626901aa33ce7ea2b3
sub <s3238s@naver.com>
2023-08-29 11:57:16 +0900

    230829/kopis dags 일부 수정 + execution_date 반영

--- dags/KOPIS/get_kopis.py
+++ dags/KOPIS/get_kopis.py
@@ -8,9 +8,7 @@ import pendulum
 import requests
 
 local_tz = pendulum.timezone("Asia/Seoul")
-
 SERVER_API = Variable.get("SERVER_API")
-ST_DT = datetime.now().strftime('%Y-%m-%d')
 
 default_args = {
     'owner': 'sms',
@@ -26,12 +24,39 @@ dag = DAG(
     user_defined_macros={'local_dt': lambda execution_date: execution_date.in_timezone(local_tz).strftime("%Y-%m-%d %H:%M:%S")},
 )
 
+#DB 적재
+def load_db(execution_date,**context):
+    exe_dt=execution_date.in_timezone('Asia/Seoul').strftime('%Y-%m-%d')
+    api_url = f"http://{SERVER_API}/kopis/performance-to-db?date={exe_dt}"
 
-def load_db(**context):
-    api_url = f"http://{SERVER_API}/kopis/performance-to-db/"
     response = requests.get(api_url).json()
 
+    print(f"{exe_dt}일, {response}건 performance DB 적재 완료")
+
     return response
+
+#정합성  체크
+def check_logic(execution_date,**context):
+    exe_dt=execution_date.in_timezone('Asia/Seoul').strftime('%Y-%m-%d')
+    DB_CNT=context['task_instance'].xcom_pull(task_ids='Load.Kopis_to_DB')
+    api_url = f"http://{SERVER_API}/check/kopis?st_dt={exe_dt}&db_cnt={DB_CNT}"
+    request = requests.get(api_url)
+
+    if request == 1 :
+        return "ERROR"
+    
+    else:
+        return "DONE"
+
+def get_detail(execution_date):
+    exe_dt=execution_date.in_timezone('Asia/Seoul').strftime('%Y-%m-%d')
+    api_url = f"http://{SERVER_API}/kopis/information?date={exe_dt}"
+    request = requests.get(api_url)
+
+    print(f"{exe_dt}일, {len(request)}건 공연상세정보 적재 완료")
+    print(request)
+
+
 
 start = EmptyOperator(task_id = 'Stark.task', dag = dag)
 finish = EmptyOperator(task_id = 'Finish.task', dag = dag)
@@ -40,23 +65,10 @@ load_to_db = PythonOperator(task_id="Load.Kopis_to_DB",
                             python_callable=load_db,
                             dag=dag)
 
-save_detail = BashOperator(task_id="Save.Kopis_Detail",
-                            bash_command=f"curl {SERVER_API}/kopis/information",
+save_detail = PythonOperator(task_id="Save.Kopis_Detail",
+                            python_callable=get_detail,
                             dag=dag)
 
-
-#정합성  체크
-def check_logic(**context):
-    DB_CNT=context['task_instance'].xcom_pull(task_ids='Load.Kopis_to_DB')
-    api_url = f"http://{SERVER_API}/check/kopis?st_dt={ST_DT}&db_cnt={DB_CNT}"
-    request = requests.get(api_url)
-
-    if request == 1 :
-        return "ERROR"
-    
-    else:
-        return "DONE"
-    
 branching = BranchPythonOperator(task_id='Check.logic',
                                  python_callable=check_logic,
                                  dag=dag)
