8729260cc0f24a5ea4d712f5071969139ec46ef4
J3llyBe4n <zza0423@gmail.com>
2023-08-30 11:18:46 +0900

    230830/yoda IMDB API 콜 DAG 테스트

--- dags/IMDB/get_academy_data.py
+++ dags/IMDB/get_academy_data.py
@@ -5,44 +5,41 @@ from airflow.operators.python import PythonOperator
 from airflow.operators.empty import EmptyOperator
 from airflow.models.variable import Variable
 import pendulum, requests, json, os
+from datetime import datetime
+
+KST = pendulum.timezone('Asia/Seoul')
 
 default_args = {
 	'owner': 'v0.3.0/yoda/dev',
 	'depends_on_past' : False,
-	'start_date': datetime(1930,5,1, tzinfo=KST)}
+	'start_date': datetime(1930,1,1, tzinfo=KST)}
 
 dag = DAG('get_academy_datas', default_args = default_args, max_active_runs= 1, tags=['수집','IMDB', 'academy'] , schedule_interval= '20 0 10 5 *')
 
-def gen_curl_operator(name: str, url : str, trigger_opt: str):
-    #curl 싸개
-
-    curl_cmd = """
-        curl "link"
-    """
-
-    curl_cmd = curl_cmd.replace("link", url)
-
-    bash_task = BashOperator(
-        task_id=name,
-        bash_command=curl_cmd,
-        trigger_rule = trigger_opt,
-        dag=dag
-    )
-
-    return bash_task
+def send_req(event, exe_year, base_url):
+	import subprocess
+	curl_url = f"{base_url}?event={event}&year={exe_year}"
+	command = ["curl", curl_url]
+	subprocess.run(command)
 
 start_task = EmptyOperator(
 	task_id = 'start_academy_data_task',
 	dag = dag)
 
-get_data = BashOperator(
+load_datas = PythonOperator(
 	task_id = 'get_academy_datas',
-	bash_command = "curl '192.168.90.128:4551/imdb/award?event=academy&year={{next_execution_date}}'",
-	dag = dag)
+	python_callable = send_req,
+	op_args=['academy', '{{next_execution_date.strftime("%Y")}}','192.168.90.128:4551/imdb/award'],
+	dag = dag
+	)
 
+check_datas = PythonOperator(
+	task_id = 'check_academy_datas',
+	python_callable = send_req,
+	op_args =['academy','{{next_execution_date.strftime("%Y")}}','192.168.90.128:4551/check/imdb'])
 
 end_task = EmptyOperator(
 	task_id = 'finish_academy_data_task',
 	dag = dag)
 
-start_task >> get_data >> end_task
+start_task >> load_datas >> check_datas >> end_task
