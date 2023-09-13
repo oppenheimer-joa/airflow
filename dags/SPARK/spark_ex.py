341d1b2572941434c8df7f9ff49f00ef74898e7f
woorek <lkjwj95@gmail.com>
2023-09-13 12:19:21 +0900

    230913/woorek vpn 활/비활성화 test스크립트 작성

--- dags/SPARK/spark_ex.py
+++ dags/SPARK/spark_ex.py
@@ -1,60 +1,71 @@
 from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.operators.bash import BashOperator
+from airflow.providers.ssh.hooks.ssh import SSHHook
+from airflow.providers.ssh.operators.ssh import SSHOperator
 from airflow.operators.empty import EmptyOperator
 from airflow.models.variable import Variable
-import pendulum, requests, json, os
-from datetime import datetime
+import pendulum
 
 
 KST = pendulum.timezone('Asia/Seoul')
-SMS_1 = Variable.get("SMS_1")
-OPENVPN = Variable.get("OPENVPN")
 # SERVER_API = Variable.get("SERVER_API")
 DAGS_OWNER = Variable.get('DAGS_OWNER')
 
-
 default_args = {
-	'owner': DAGS_OWNER,
-	'depends_on_past': True,
-	'start_date':datetime(1930, 1, 1, tzinfo=KST)}
+    'owner': DAGS_OWNER,
+    'depends_on_past': True,
+    'start_date':datetime(2023, 9, 11, tzinfo=KST)}
 
-dag = DAG('spark_test',
-	  default_args = default_args,
+dag = DAG('spark_ex',
+      default_args = default_args,
       max_active_runs= 1,
       tags=['스파크 테스트'],
-      schedule_interval= '@once')   ## 매년 5월 1일 AM 11:00 실행
+      schedule_interval= '@once')
+
+sshHook1 = SSHHook(ssh_conn_id='sms-1')
+sshHook2 = SSHHook(ssh_conn_id='sms-1-vpn', cmd_timeout=15)
 
 
 start_task = EmptyOperator(
-	task_id = 'start_task',
-	dag = dag)
+    task_id = 'start_task',
+    dag = dag)
 
 
-activate_vpn = BashOperator(
-	task_id = 'activate_openvpn',
-	bash_command = """
-	ssh -i /home/airflow/.ssh/sms-1.pem ubuntu@{SMS_1} 'sh /home/ubuntu/openvpn/activate.sh'
-	""",
-	dag=dag)
+activate_task = SSHOperator(
+    task_id = 'activate_vpn',
+    command = 'sh /home/ubuntu/openvpn/activate_vpn.sh ',
+    ssh_hook = sshHook1,
+    do_xcom_push=False,
+    dag=dag)
 
-test_task = BashOperator(
-	task_id = 'test_work',
-	bash_command = """
-	ssh -i /home/airflow/.ssh/sms-1.pem ubuntu@{OPENVPN} 'echo testtttt'
-	""",
-	dag=dag)
+t1 = SSHOperator(
+    task_id = 'sh_test',
+    command = "ssh sparkserver 'sh /home/spark/test.sh' ",
+    ssh_hook = sshHook2,
+    do_xcom_push=False,
+    dag=dag)
 
-deactivate_vpn = BashOperator(
-	task_id = 'deactivate_openvpn',
-	bash_command = """
-	ssh -i /home/airflow/.ssh/sms-1.pem ubuntu@{OPENVPN} 'sh /home/ubuntu/openvpn/deactivate.sh'
-	""",
-	dag=dag)
+deactivate_task = SSHOperator(
+    task_id = 'deactivate_vpn',
+    command = 'sh /home/ubuntu/openvpn/deactivate_vpn.sh ',
+    # command = 'sudo pkill openvpn ',
+    ssh_hook = sshHook2,
+    do_xcom_push=False,
+    get_pty=True,
+    dag=dag)
+
+
+check_connection = BashOperator(
+    task_id = 'check_original_ip_conn',
+    bash_command = "nc -z -w 1 43.202.116.246 80 && echo '연결 성공' || echo '연결 실패' ",
+    trigger_rule = 'none_skipped',
+    dag=dag)
+
 
 end_task = EmptyOperator(
-	task_id = 'finish_task',
-	dag = dag)
+    task_id = 'finish_task',
+    trigger_rule='all_done',
+    dag = dag)
 
-
-start_task >> activate_vpn >> test_task >> deactivate_vpn >> end_task
+start_task >> activate_task >> t1 >> deactivate_task >> check_connection >> end_task
